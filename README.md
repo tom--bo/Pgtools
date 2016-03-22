e0ec11f3040d348388daf048492e0fc90e378f1a
tom--bo <otsuka.t.2013@gmail.com>
2016-03-22 23:22:54 +0900

    First commit. Start with Minila.

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Pgtools - It's new $module
+
+# SYNOPSIS
+
+    use Pgtools;
+
+# DESCRIPTION
+
+Pgtools is ...
+
+# LICENSE
+
+Copyright (C) tom--bo.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+tom--bo &lt;otsuka.t.2013@gmail.com>
---
+++ lib/Pgtools.pm
@@ -0,0 +1,39 @@
+package Pgtools;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Pgtools - It's new $module
+
+=head1 SYNOPSIS
+
+    use Pgtools;
+
+=head1 DESCRIPTION
+
+Pgtools is ...
+
+=head1 LICENSE
+
+Copyright (C) tom--bo.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+tom--bo E<lt>otsuka.t.2013@gmail.comE<gt>
+
+=cut
+
